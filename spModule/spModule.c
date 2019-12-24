16590c86e59ee491a69bed2e430912cf3414ac94
XMX <xmx1025@gmail.com>
2019-12-24 15:19:47 +0900

    Modified print format

--- spModule/spModule.c
+++ spModule/spModule.c
@@ -23,14 +23,12 @@ unsigned short int forwardingPort = 0;
 
 static int proc_open(struct inode *inode, struct file *file)
 {
-	printk(KERN_INFO "[ MOD-LOG ] Proc Opened.\n");
-	if (1 != htonl(1)) printk(KERN_INFO "[ MOD-LOG ]   Little Endian\n");
-	else printk(KERN_INFO "[MOD-LOG]   Big Endian\n");
+	printk(KERN_INFO "Proc Opened.\n");
 	return 0;
 }
 static ssize_t proc_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *ppos)
 {	sscanf(user_buffer, "%hu", &forwardingPort);
-	printk(KERN_INFO "[ MOD-LOG ] Proc Wrote: %hu\n", forwardingPort);
+	printk(KERN_INFO "Proc Wrote: %hu\n", forwardingPort);
 	return count;
 }
 static const struct file_operations proc_fops = {
@@ -43,28 +41,28 @@ struct iphdr *IPH;
 struct tcphdr *TCPH;
 unsigned short int sport;
 unsigned short int dport;
-static int tag = 0;
+static int isTCP = 0;
 static unsigned int hook_pre(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	IPH = ip_hdr(skb);
-	TCPH = tcp_hdr(skb);
-	sport = ntohs((unsigned short int)TCPH->source);
-	dport = ntohs((unsigned short int)TCPH->dest);
-
-	tag ++;
-	printk(KERN_INFO "[ MOD-LOG ] <%d>PreHook >> syn:%hu ack:%hu fin:%hu\n", tag, TCPH->syn, TCPH->ack, TCPH->fin);
-	printk(KERN_INFO "[ MOD-LOG ]   sIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->saddr), sport);
-	printk(KERN_INFO "[ MOD-LOG ]   dIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->daddr), dport);
-
-	if (dport == forwardingPort)
+	isTCP = 0;
+	if (IPH->protocol == IPPROTO_TCP)
 	{
-		printk("[ MOD-LOG ]     Forwarding #%hu >>\n", forwardingPort);
-		IPH->saddr = in_aton("131.1.1.0");
-		IPH->daddr = in_aton("123.1.1.0");
-		TCPH->dest = htons((unsigned short int)7777);
+		isTCP = 1;
+		TCPH = tcp_hdr(skb);
+		sport = ntohs((unsigned short int)TCPH->source);
 		dport = ntohs((unsigned short int)TCPH->dest);
-		printk(KERN_INFO "[ MOD-LOG ]       sIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->saddr), sport);
-		printk(KERN_INFO "[ MOD-LOG ]       dIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->daddr), dport);
+	}
+
+	printk(KERN_INFO "PRE_ROUTING[(%s; %hu; %hu; %d.%d.%d.%d; %d.%d.%d.%d)]", isTCP ? "TCP" : "Non-TCP", sport, dport, NIPQUAD(IPH->saddr), NIPQUAD(IPH->daddr));
+	
+	if (IPH->protocol == IPPROTO_TCP && sport == forwardingPort)
+	{
+		IPH->daddr = in_aton("131.1.1.0");
+		TCPH->dest = htons((unsigned short int)7777);
+		TCPH->source = htons((unsigned short int)7777);
+		sport = ntohs((unsigned short int)TCPH->source);
+		dport = ntohs((unsigned short int)TCPH->dest);
 
 		// Update checksum
 		TCPH->check = 0;
@@ -82,58 +80,38 @@ static unsigned int hook_pre(void *priv, struct sk_buff *skb, const struct nf_ho
 static unsigned int hook_post(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	IPH = ip_hdr(skb);
-	TCPH = tcp_hdr(skb);
-	sport = ntohs((unsigned short int)TCPH->source);
-	dport = ntohs((unsigned short int)TCPH->dest);
+	isTCP = 0;
+	if (IPH->protocol == IPPROTO_TCP)
+	{
+		isTCP = 1;
+		TCPH = tcp_hdr(skb);
+		sport = ntohs((unsigned short int)TCPH->source);
+		dport = ntohs((unsigned short int)TCPH->dest);
+	}
 
-	tag ++;
-	printk(KERN_INFO "[ MOD-LOG ] <%d>PostHook >> syn:%hu ack:%hu fin:%hu\n", tag, TCPH->syn, TCPH->ack, TCPH->fin);
-	printk(KERN_INFO "[ MOD-LOG ]   sIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->saddr), sport);
-	printk(KERN_INFO "[ MOD-LOG ]   dIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->daddr), dport);
+	printk(KERN_INFO "POST_ROUTING[(%s; %hu; %hu; %d.%d.%d.%d; %d.%d.%d.%d)]", isTCP ? "TCP" : "Non-TCP", sport, dport, NIPQUAD(IPH->saddr), NIPQUAD(IPH->daddr));
 	return NF_ACCEPT;
 }
 
-
-static unsigned int hook_out(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
-{
-	IPH = ip_hdr(skb);
-	TCPH = tcp_hdr(skb);
-	sport = ntohs((unsigned short int)TCPH->source);
-	dport = ntohs((unsigned short int)TCPH->dest);
-	tag ++;
-	printk(KERN_INFO "[ MOD-LOG ] <%d>OutHook >> syn:%hu ack:%hu fin:%hu\n", tag, TCPH->syn, TCPH->ack, TCPH->fin);
-	printk(KERN_INFO "[ MOD-LOG ]   sIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->saddr), sport);
-	printk(KERN_INFO "[ MOD-LOG ]   dIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->daddr), dport);
-	return NF_ACCEPT;
-}
-static unsigned int hook_in(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
-{
-	IPH = ip_hdr(skb);
-	TCPH = tcp_hdr(skb);
-	sport = ntohs((unsigned short int)TCPH->source);
-	dport = ntohs((unsigned short int)TCPH->dest);
-	tag ++;
-	printk(KERN_INFO "[ MOD-LOG ] <%d>InHook >> syn:%hu ack:%hu fin:%hu\n", tag, TCPH->syn, TCPH->ack, TCPH->fin);
-	printk(KERN_INFO "[ MOD-LOG ]   sIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->saddr), sport);
-	printk(KERN_INFO "[ MOD-LOG ]   dIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->daddr), dport);
-	return NF_ACCEPT;
-}
 static unsigned int hook_fwrd(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
 	IPH = ip_hdr(skb);
-	TCPH = tcp_hdr(skb);
-	sport = ntohs((unsigned short int)TCPH->source);
-	dport = ntohs((unsigned short int)TCPH->dest);
-	tag ++;
-	printk(KERN_INFO "[ MOD-LOG ] <%d>FowardHook >> syn:%hu ack:%hu fin:%hu\n", tag, TCPH->syn, TCPH->ack, TCPH->fin);
-	printk(KERN_INFO "[ MOD-LOG ]   sIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->saddr), sport);
-	printk(KERN_INFO "[ MOD-LOG ]   dIP: %d.%d.%d.%d <%hu>\n", NIPQUAD(IPH->daddr), dport);
+	isTCP = 0;
+	if (IPH->protocol == IPPROTO_TCP)
+	{
+		isTCP = 1;
+		TCPH = tcp_hdr(skb);
+		sport = ntohs((unsigned short int)TCPH->source);
+		dport = ntohs((unsigned short int)TCPH->dest);
+	}
+
+	printk(KERN_INFO "FORWARD[(%s; %hu; %hu; %d.%d.%d.%d; %d.%d.%d.%d)]", isTCP ? "TCP" : "Non-TCP", sport, dport, NIPQUAD(IPH->saddr), NIPQUAD(IPH->daddr));
+
 	return NF_ACCEPT;
 }
+
 static struct nf_hook_ops ops_pre;
 static struct nf_hook_ops ops_post;
-static struct nf_hook_ops ops_out;
-static struct nf_hook_ops ops_in;
 static struct nf_hook_ops ops_fwrd;
 
 static int __init mod_init(void)
@@ -148,17 +126,7 @@ static int __init mod_init(void)
 	ops_post.pf = PF_INET;
 	ops_post.hooknum= NF_INET_POST_ROUTING;
 	ops_post.priority = NF_IP_PRI_FIRST;
-	// Hook: Local Out
-	ops_out.hook = hook_out;
-	ops_out.pf = PF_INET;
-	ops_out.hooknum = NF_INET_LOCAL_OUT;
-	ops_out.priority = NF_IP_PRI_FIRST;
-	// Hook: Local In
-	ops_in.hook = hook_in;
-	ops_in.pf = PF_INET;
-	ops_in.hooknum = NF_INET_LOCAL_IN;
-	ops_in.priority = NF_IP_PRI_FIRST;
-	// Hook: Forward
+	// Hook: Pre Routing
 	ops_fwrd.hook = hook_fwrd;
 	ops_fwrd.pf = PF_INET;
 	ops_fwrd.hooknum = NF_INET_FORWARD;
@@ -166,13 +134,11 @@ static int __init mod_init(void)
 
 	// Add Proc
 	proc_entry = proc_create(ENTRY_NAME, 0755, NULL, &proc_fops);
-	printk(KERN_INFO "[ MOD-LOG ] Module Loaded.\n");
+	printk(KERN_INFO "Module Loaded.\n");
 
 	// Register hooks
 	nf_register_hook(&ops_pre);
 	nf_register_hook(&ops_post);
-	nf_register_hook(&ops_out);
-	nf_register_hook(&ops_in);
 	nf_register_hook(&ops_fwrd);
 
 	return 0;
@@ -182,13 +148,11 @@ static void __exit mod_exit(void)
 {
 	// Remove Proc
 	proc_remove(proc_entry);
-	printk(KERN_INFO "[ MOD-LOG ] Module Exited.");
+	printk(KERN_INFO "Module Exited.");
 
 	// Unregister hooks
 	nf_unregister_hook(&ops_pre);
 	nf_unregister_hook(&ops_post);
-	nf_unregister_hook(&ops_out);
-	nf_unregister_hook(&ops_in);
 	nf_unregister_hook(&ops_fwrd);
 }
 
@@ -199,4 +163,3 @@ MODULE_AUTHOR("Team 7");
 MODULE_DESCRIPTION("SP Mod: Netfilter");
 MODULE_LICENSE("GLP");
 MODULE_VERSION("0.0");
-
